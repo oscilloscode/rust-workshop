67dc17005345bd63e9ada11493e86d3746debc91
iot <iot@clt-mob-t-6401>
2024-04-12 11:07:26 +0200

    day4: fix more lints

--- day_4/badlib/src/lib.rs
+++ day_4/badlib/src/lib.rs
@@ -5,21 +5,26 @@
 //! We will incrementally build a CI pipeline to discover
 //! how CI can help us make sure our Rust code is squeaky clean.
 
+use std::num::TryFromIntError;
+
+/// This used to be a badly formatted function, but not anymore thanks to CI.
 pub fn badly_formatted() {
     println!("This code is badly formatted.");
     println!("Don't change it until CI fails because of it.");
 }
 
-pub fn dangerous_cast(n: i64) -> i32 {
+/// This cast is not dangerous anymore!
+pub fn dangerous_cast(n: i64) -> Result<i32, TryFromIntError> {
     // In my opinion, the `as` keyword should be used sparingly.
     // This conversion is lossy and may not do what you expect.
     // It is better to replace it with `.try_into()` and return
     // a `Reslut<i64, _>` instead.
     //
     // But as always, let's get CI to complain about it before we fix it.
-    n as i32
+    n.try_into()
 }
 
+/// This function is adding numbers (wrongly).
 pub fn add(left: usize, right: usize) -> usize {
     // I don't know how to add two numbers...
     left - right
